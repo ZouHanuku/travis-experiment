0db18e9d63ebade5e3f4143d043adb32a9ec8952
Marina Delas <marina.delas@gmail.com>
2018-06-30 23:07:43 +0200

    tests are ok for math basics

---
+++ multiply.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/*
+ * Create a function `multiply` that takes two number arguments
+ * and returns the result of the multiplication of those two.
+ * But you must do this without using the operators * or /
+ *
+ * @notions Primitive and Operators, Functions, While
+ */
+
+// Your code :
+const multiply = (a, b) => {
+    let result = 0;
+    for (let i = 0; i < Math.abs(b); i++){
+        result += a;
+    }
+    return (b >= 0) ? result : -result;
+}
+//* Begin of tests
+const assert = require('assert')
+
+assert.strictEqual(typeof multiply, 'function')
+assert.strictEqual(multiply.length, 2)
+assert.strictEqual(multiply.toString().includes('Math.imul'), false)
+assert.strictEqual(multiply.toString().includes('*'), false)
+assert.strictEqual(multiply.toString().includes('/'), false)
+assert.strictEqual(multiply(34, 78), 2652)
+assert.strictEqual(multiply(123, 0), 0)
+assert.strictEqual(multiply(0, -230), 0)
+assert.strictEqual(multiply(0, 0), 0)
+assert.strictEqual(multiply(123, -22), -2706)
+assert.strictEqual(multiply(-22, 123), -2706)
+assert.strictEqual(multiply(-22, -123), 2706)
+// End of tests */
